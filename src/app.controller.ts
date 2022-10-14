db96706c24bc8fcd62cef0308a83fc5a99e7ba72
Wanderson Santos <81826043+wandersonDeve@users.noreply.github.com>
2022-10-14 01:24:47 -0300

    chore: add lib rabbitmq and connect to send messag

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { AmqpConnection } from '@golevelup/nestjs-rabbitmq';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private amqpConnection: AmqpConnection,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('publish')
+  publiher() {
+    this.amqpConnection.publish('amq.direct', 'pagamentos', {
+      client: 'Wanderson Santos',
+      amount: 10000,
+    });
+
+    console.log('mensagem publicada');
+
+    return 'mensagem publicada';
   }
 }
